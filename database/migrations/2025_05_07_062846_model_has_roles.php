8a694b3781a89ca5497e21de793cf34d51ae010a
darklco <darkvlcov@gmail.com>
2025-05-16 22:31:55 +0700

    memperbaiki migration

--- database/migrations/2025_05_07_062846_model_has_roles.php
+++ database/migrations/2025_05_07_062846_model_has_roles.php
@@ -12,17 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('model_has_roles', function (Blueprint $table) {
-            $table->unsignedBigInteger('roles_id');
+            $table->unsignedBigInteger('role_id');
             $table->string('model_type');
             $table->unsignedBigInteger('model_id');
 
             $table->primary(['role_id', 'model_id', 'model_type']);
             $table->index(['model_id', 'model_type']);
-            
-            $table->foreign('roles_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
+
+            // foreign key harus cocok dengan nama kolom: role_id
+            $table->foreign('role_id')
+                  ->references('id')
+                  ->on('roles')
+                  ->onDelete('cascade');
         });
     }
 
